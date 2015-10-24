db143fb9ffb5283200ca0cdba488ecb2a2f266e5
Aidin Gharibnavaz <aidin@aidinhut.com>
2015-10-24 17:45:17 +0330

    Now it generates a random IV and put it in the message.
    Instead of a hard-coded IV, which is unsecure.

--- app/src/main/java/com/aidinhut/simpletextcrypt/Crypter.java
+++ app/src/main/java/com/aidinhut/simpletextcrypt/Crypter.java
@@ -6,6 +6,7 @@ import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -19,8 +20,6 @@ import javax.crypto.spec.SecretKeySpec;
  */
 public class Crypter {
 
-    private static String ivKey = "Rt9832jdnc@3kfP.";
-
     public static String encrypt(String encKey, String input)
         throws UnsupportedEncodingException,
                NoSuchAlgorithmException,
@@ -30,6 +29,12 @@ public class Crypter {
                IllegalBlockSizeException,
                BadPaddingException
     {
+        // IV (Initialization Vector) generates randomly, and sends along with the message.
+        // Since we use CBC mode, 1. IV *must* be unique in every message 2. IV does not need
+        // to be secret.
+        // See: https://stackoverflow.com/questions/3436864/sending-iv-along-with-cipher-text-safe
+
+        String ivKey = getRandomIV();
         IvParameterSpec iv = new IvParameterSpec(ivKey.getBytes("UTF-8"));
 
         SecretKeySpec skeySpec = new SecretKeySpec(encKey.getBytes("UTF-8"), "AES");
@@ -38,7 +43,7 @@ public class Crypter {
 
         byte[] encrypted = cipher.doFinal(input.getBytes());
 
-        return Base64.encodeToString(encrypted, Base64.DEFAULT);
+        return ivKey + Base64.encodeToString(encrypted, Base64.DEFAULT);
     }
 
     public static String decrypt(String encKey, String input)
@@ -50,14 +55,29 @@ public class Crypter {
                IllegalBlockSizeException,
                BadPaddingException
     {
+        // First 16 chars is the random IV.
+        String ivKey = input.substring(0, 16);
+        String encryptedMessage = input.substring(16);
+
         IvParameterSpec iv = new IvParameterSpec(ivKey.getBytes("UTF-8"));
 
         SecretKeySpec skeySpec = new SecretKeySpec(encKey.getBytes("UTF-8"), "AES");
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
         cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
 
-        byte[] original = cipher.doFinal(Base64.decode(input, Base64.DEFAULT));
+        byte[] original = cipher.doFinal(Base64.decode(encryptedMessage, Base64.DEFAULT));
 
         return new String(original);
+    }
+
+    private static String getRandomIV() {
+        Random random = new Random();
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < 16; ++i) {
+            builder.append((char)(random.nextInt(96) + 32));
+        }
+
+        return builder.toString();
     }
 }
